f615a8ee94550d6f45300c30d92295fa38dd8c7b
Adam Fraser <adamf@couchbase.com>
2017-05-25 23:23:52 -0700

    Uptake change to WriteUpdateWithXattr signature

--- crud.go
+++ crud.go
@@ -348,8 +348,8 @@ func (bucket *WalrusBucket) DeleteWithXattr(k string, xattrKey string) error {
 	return errors.New("DeleteWithXattr not implemented for walrus")
 }
 
-func (bucket *WalrusBucket) WriteUpdateWithXattr(k string, xattrKey string, exp int, callback sgbucket.WriteUpdateWithXattrFunc) error {
-	return errors.New("WriteUpdateWithXattr not implemented for walrus")
+func (bucket *WalrusBucket) WriteUpdateWithXattr(k string, xattrKey string, exp int, callback sgbucket.WriteUpdateWithXattrFunc) (casOut uint64, err error) {
+	return 0, errors.New("WriteUpdateWithXattr not implemented for walrus")
 }
 
 func (bucket *WalrusBucket) getData(v interface{}, isJSON bool) (data []byte, err error) {
